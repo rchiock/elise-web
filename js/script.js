0d69516232510561e447aa546f04c2fff8a72922
Ricardo <rchiock@level715.com>
2016-03-22 18:04:20 -0500

    por revisar

--- js/script.js
+++ js/script.js
@@ -11,13 +11,13 @@
   'use strict';
 
   if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-    var msViewportStyle = document.createElement('style')
-    msViewportStyle.appendChild(
-      document.createTextNode(
-        '@-ms-viewport{width:auto!important}'
-      )
-    )
-    document.querySelector('head').appendChild(msViewportStyle)
+	var msViewportStyle = document.createElement('style')
+	msViewportStyle.appendChild(
+	  document.createTextNode(
+		'@-ms-viewport{width:auto!important}'
+	  )
+	)
+	document.querySelector('head').appendChild(msViewportStyle)
   }
 
 })();
@@ -30,15 +30,15 @@ $(document).on('change', '.btn-file :file', function() {
 });
 
 $(document).ready( function() {
-		$('.btn-file :file').on('fileselect', function(event, numFiles, label) {
-				var input = $(this).parents('.input-group').find(':text'),
-						log = numFiles > 1 ? numFiles + ' files selected' : label;	
-				if( input.length ) {
-						input.val(log);
-				} else {
-						if( log ) alert(log);
-				}	
-		});
+	$('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+			var input = $(this).parents('.input-group').find(':text'),
+					log = numFiles > 1 ? numFiles + ' files selected' : label;	
+			if( input.length ) {
+					input.val(log);
+			} else {
+					if( log ) alert(log);
+			}	
+	});
 
 
 	/* Anchors Scroll to ID
@@ -81,5 +81,35 @@ $(document).ready( function() {
 	})
 
 	$("a.lightbox").fancybox();
+
+	/* Replace all SVG images with inline SVG */
+	jQuery('img.svg').each(function(){
+		var $img = jQuery(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+
+		jQuery.get(imgURL, function(data) {
+			// Get the SVG tag, ignore the rest
+			var $svg = jQuery(data).find('svg');
+
+			// Add replaced image's ID to the new SVG
+			if(typeof imgID !== 'undefined') {
+				$svg = $svg.attr('id', imgID);
+			}
+			// Add replaced image's classes to the new SVG
+			if(typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+			}
+
+			// Remove any invalid XML tags as per http://validator.w3.org
+			$svg = $svg.removeAttr('xmlns:a');
+
+			// Replace image with new SVG
+			$img.replaceWith($svg);
+
+		}, 'xml');
+
+	});
 
 });
